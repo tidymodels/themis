0437d8ba98b401314c941ea07eb5b46180fcae68
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2019-10-11 22:18:35 -0700

    add upsample and downsample tests

---
+++ R/0_imports.R
@@ -0,0 +1,1 @@
+#' @importFrom tidyselect everything
--- tests/testthat/test-downsample.R
+++ tests/testthat/test-downsample.R
@@ -2,8 +2,8 @@ library(testthat)
 library(recipes)
 library(dplyr)
 
+context("Down-sampling")
 
-context("Upsampling")
 
 iris2 <- iris[-(1:45),]
 iris2$Species[seq(6, 96, by = 5)] <- NA
@@ -14,10 +14,11 @@ rec <- recipe( ~ ., data = iris2)
 
 test_that('basic usage', {
   rec1 <- rec %>%
-    step_upsample(tidyselect::matches("Species$"), id = "")
+    step_downsample(tidyselect::matches("Species$"), id = "")
 
   untrained <- tibble(
-    terms = "tidyselect::matches(\"Species$\")", id = ""
+    terms = "tidyselect::matches(\"Species$\")",
+    id = ""
   )
 
   expect_equivalent(untrained, tidy(rec1, number = 1))
@@ -31,12 +32,13 @@ test_that('basic usage', {
 
   expect_equal(trained, tidy(rec1_p, number = 1))
 
+
   tr_xtab <- table(juice(rec1_p)$Species, useNA = "always")
   te_xtab <- table(bake(rec1_p, new_data = iris2)$Species, useNA = "always")
   og_xtab <- table(iris2$Species, useNA = "always")
 
-  expect_equal(max(tr_xtab), max(og_xtab))
-  expect_equal(sum(is.na(juice(rec1_p)$Species)), max(og_xtab))
+  expect_equal(max(tr_xtab), 5)
+  expect_equal(sum(is.na(juice(rec1_p)$Species)), 5)
   expect_equal(te_xtab, og_xtab)
 
   expect_warning(prep(rec1, training = iris2), NA)
@@ -44,7 +46,7 @@ test_that('basic usage', {
 
 test_that('ratio value', {
   rec2 <- rec %>%
-    step_upsample(tidyselect::matches("Species$"), ratio = .25)
+    step_downsample(tidyselect::matches("Species$"), under_ratio = 2)
 
   rec2_p <- prep(rec2, training = iris2, retain = TRUE)
 
@@ -52,16 +54,15 @@ test_that('ratio value', {
   te_xtab <- table(bake(rec2_p, new_data = iris2)$Species, useNA = "always")
   og_xtab <- table(iris2$Species, useNA = "always")
 
-  expect_equal(min(tr_xtab), 10)
-  expect_equal(sum(is.na(juice(rec2_p)$Species)),
-               sum(is.na(iris2$Species)))
+  expect_equal(max(tr_xtab), 10)
+  expect_equal(sum(is.na(juice(rec2_p)$Species)), 10)
   expect_equal(te_xtab, og_xtab)
 })
 
 
 test_that('no skipping', {
   rec3 <- rec %>%
-    step_upsample(tidyselect::matches("Species$"), skip = FALSE)
+    step_downsample(tidyselect::matches("Species$"), skip = FALSE)
 
   rec3_p <- prep(rec3, training = iris2, retain = TRUE)
 
@@ -69,7 +70,7 @@ test_that('no skipping', {
   te_xtab <- table(bake(rec3_p, new_data = iris2)$Species, useNA = "always")
   og_xtab <- table(iris2$Species, useNA = "always")
 
-  expect_equal(max(tr_xtab), max(og_xtab))
+  expect_equal(max(tr_xtab), 5)
   expect_equal(te_xtab, tr_xtab)
 })
 
@@ -78,24 +79,24 @@ test_that('no skipping', {
 test_that('bad data', {
   expect_error(
     rec %>%
-      step_upsample(Sepal.Width) %>%
+      step_downsample(Sepal.Width) %>%
       prep(retain = TRUE)
   )
   expect_error(
     rec %>%
-      step_upsample(Species3) %>%
+      step_downsample(Species3) %>%
       prep(strings_as_factors = FALSE, retain = TRUE)
   )
   expect_error(
     rec %>%
-      step_upsample(Species, Species2) %>%
+      step_downsample(Species, Species2) %>%
       prep(strings_as_factors = FALSE, retain = TRUE)
   )
 })
 
 test_that('printing', {
   rec4 <- rec %>%
-    step_upsample(Species)
+    step_downsample(Species)
 
   expect_output(print(rec))
   expect_output(prep(rec4, training = iris2, retain = TRUE, verbose = TRUE))
@@ -103,17 +104,17 @@ test_that('printing', {
 
 test_that('`seed` produces identical sampling', {
 
-  upsample_with_seed <- function(rec, seed = sample.int(10^5, 1)) {
+  downsample_with_seed <- function(rec, seed = sample.int(10^5, 1)) {
     rec %>%
-      step_upsample(Species, seed = seed) %>%
+      step_downsample(Species, seed = seed) %>%
       prep(training = iris2, retain = TRUE) %>%
       juice() %>%
       pull(Petal.Width)
   }
 
-  petal_width_1 <- upsample_with_seed(rec, seed = 1234)
-  petal_width_2 <- upsample_with_seed(rec, seed = 1234)
-  petal_width_3 <- upsample_with_seed(rec, seed = 12345)
+  petal_width_1 <- downsample_with_seed(rec, seed = 1234)
+  petal_width_2 <- downsample_with_seed(rec, seed = 1234)
+  petal_width_3 <- downsample_with_seed(rec, seed = 12345)
 
   expect_equal(petal_width_1, petal_width_2)
   expect_false(identical(petal_width_1, petal_width_3))
@@ -125,10 +126,10 @@ test_that('ratio deprecation', {
   expect_message(
     new_rec <-
       rec %>%
-      step_upsample(tidyselect::matches("Species$"), ratio = 2),
+      step_downsample(tidyselect::matches("Species$"), ratio = 2),
     "argument is now deprecated"
   )
-  expect_equal(new_rec$steps[[1]]$over_ratio, 2)
+  expect_equal(new_rec$steps[[1]]$under_ratio, 2)
 })
 
 
@@ -136,9 +137,9 @@ test_that('ratio deprecation', {
 test_that('tunable', {
   rec <-
     recipe(~ ., data = iris) %>%
-    step_upsample(all_predictors())
-  rec_param <- tunable.step_upsample(rec$steps[[1]])
-  expect_equal(rec_param$name, c("over_ratio"))
+    step_downsample(all_predictors(), under_ratio = 1)
+  rec_param <- tunable.step_downsample(rec$steps[[1]])
+  expect_equal(rec_param$name, c("under_ratio"))
   expect_true(all(rec_param$source == "recipe"))
   expect_true(is.list(rec_param$call_info))
   expect_equal(nrow(rec_param), 1)
