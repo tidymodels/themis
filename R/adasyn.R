ad2ec977b060ec3571de459c5e1c677600d94f6f
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2019-10-26 14:34:59 -0700

    fast adasyn

--- R/adasyn.R
+++ R/adasyn.R
@@ -2,9 +2,9 @@
 #'
 #' `step_adasyn` creates a *specification* of a recipe
 #'  step that generates synthetic positive instances using ADASYN algorithm.
-#'  Using [smotefamily::ADAS()].
 #'
 #' @inheritParams recipes::step_center
+#' @inheritParams step_upsample
 #' @param ... One or more selector functions to choose which
 #'  variable is used to sample the data. See [selections()]
 #'  for more details. The selection should result in _single
@@ -57,11 +57,25 @@
 #' baked_okc <- bake(ds_rec, new_data = okc)
 #' table(baked_okc$Class, useNA = "always")
 #'
+#' library(ggplot2)
+#'
+#' ggplot(circle_example, aes(x, y, color = class)) +
+#'   geom_point() +
+#'   labs(title = "Without ADASYN")
+#'
+#' recipe(class ~ ., data = circle_example) %>%
+#'   step_adasyn(class) %>%
+#'   prep() %>%
+#'   juice() %>%
+#'   ggplot(aes(x, y, color = class)) +
+#'   geom_point() +
+#'   labs(title = "With ADASYN")
+#'
 #' @importFrom recipes rand_id add_step ellipse_check
 step_adasyn <-
   function(recipe, ..., role = NA, trained = FALSE, column = NULL,
-           neighbors = 5, skip = TRUE, seed = sample.int(10^5, 1),
-           id = rand_id("adasyn")) {
+           over_ratio = 1, neighbors = 5, skip = TRUE,
+           seed = sample.int(10^5, 1), id = rand_id("adasyn")) {
 
     add_step(recipe,
              step_adasyn_new(
@@ -69,6 +83,7 @@ step_adasyn <-
                role = role,
                trained = trained,
                column = column,
+               over_ratio = over_ratio,
                neighbors = neighbors,
                skip = skip,
                seed = seed,
@@ -78,13 +93,15 @@ step_adasyn <-
 
 #' @importFrom recipes step
 step_adasyn_new <-
-  function(terms, role, trained, column, neighbors, skip, seed, id) {
+  function(terms, role, trained, column, over_ratio, neighbors, skip, seed,
+           id) {
     step(
       subclass = "adasyn",
       terms = terms,
       role = role,
       trained = trained,
       column = column,
+      over_ratio = over_ratio,
       neighbors = neighbors,
       skip = skip,
       id = id,
@@ -117,6 +134,7 @@ prep.step_adasyn <- function(x, training, info = NULL, ...) {
     role = x$role,
     trained = TRUE,
     column = col_name,
+    over_ratio = x$over_ratio,
     neighbors = x$neighbors,
     skip = x$skip,
     seed = x$seed,
@@ -124,21 +142,8 @@ prep.step_adasyn <- function(x, training, info = NULL, ...) {
   )
 }
 
-# Turns a binary factor a variable where the majority class is coded as 0 and
-# the minority as 1.
-response_0_1 <- function(x) {
-  ifelse(x == names(sort(table(x)))[1], 1, 0)
-}
-# Turns 0-1 coded variable back into factor variable
-response_0_1_to_org <- function(old, new) {
-  ref <- names(sort(table(old)))
-  names(ref) <-c("1", "0")
-  factor(unname(ref[as.character(new)]))
-}
-
 #' @importFrom tibble as_tibble tibble
 #' @importFrom withr with_seed
-#' @importFrom smotefamily ADAS
 #' @importFrom dplyr mutate
 #' @importFrom rlang :=
 #' @export
@@ -148,14 +153,12 @@ bake.step_adasyn <- function(object, new_data, ...) {
   with_seed(
     seed = object$seed,
     code = {
-      new_data0 <-  ADAS(X = select(new_data, -!!object$column),
-                         target = new_data[[object$column]],
-                         K = object$neighbors)$data
+      new_data <- smote(new_data, object$column,
+                        k = object$neighbors, over_ratio = object$over_ratio)
     }
   )
 
-  names(new_data0)[names(new_data0) == "class"] <- object$column
-  as_tibble(new_data0[names(new_data)])
+  as_tibble(new_data)
 }
 
 #' @importFrom recipes printer terms_select
---
+++ R/adasyn_impl.R
@@ -0,0 +1,59 @@
+adasyn <- function(df, var, k = 5,  over_ratio = 1) {
+  majority_count <- max(table(df[[var]]))
+  ratio_target <- majority_count * over_ratio
+  which_upsample <-  which(table(df[[var]]) < ratio_target)
+  samples_needed <- ratio_target - table(df[[var]])[which_upsample]
+  min_names <- names(samples_needed)
+  out_dfs <- list()
+
+  data_mat <- as.matrix(df[names(df) != var])
+  ids_full <- RANN::nn2(data_mat, k = k + 1, searchtype = "priority")$nn.idx
+
+  for (i in seq_along(min_names)) {
+
+    min_class_in <- df[[var]] != min_names[i]
+
+    r_value <- pmax(
+      0,
+      rowSums(matrix((min_class_in)[ids_full],
+                     ncol = ncol(ids_full))) - 1
+    )[!min_class_in]
+
+    danger_ids <- sample(seq_along(r_value), samples_needed[i], TRUE,
+                              prob = r_value)
+
+      tmp_df <- as.data.frame(
+        adasyn_sampler(data_mat[!min_class_in, ], k, samples_needed[i],
+                   danger_ids)
+      )
+
+    colnames(tmp_df) <- colnames(data_mat)
+    tmp_df[[var]] <- min_names[i]
+    out_dfs[[i]] <- tmp_df
+  }
+  final <- rbind(do.call(rbind, out_dfs), df)
+  final[[var]] <- factor(final[[var]])
+  rownames(final) <- NULL
+  final
+}
+
+adasyn_sampler <- function(data, k, n_samples, smote_ids) {
+  ids <- RANN::nn2(data, k = k + 1, searchtype = "priority")$nn.idx
+  index_len <- tabulate(smote_ids)
+  out <- matrix(0, nrow = n_samples, ncol = ncol(data))
+  sampleids <- sample.int(k, n_samples, TRUE)
+  runif_ids <- stats::runif(n_samples)
+
+  iii <- 0
+  for (row_num in which(index_len != 0)) {
+    index_selection <- iii + seq_len(index_len[row_num])
+    # removes itself as nearest neighbour
+    id_knn <- ids[row_num, ids[row_num, ] != row_num]
+    dif <- data[id_knn[sampleids[index_selection]], ] - data[rep(row_num, index_len[row_num]), ]
+    gap <- dif * runif_ids[index_selection]
+    out[index_selection, ] <- data[rep(row_num, index_len[row_num]), ] + gap
+    iii <- iii + index_len[row_num]
+  }
+
+  out
+}
