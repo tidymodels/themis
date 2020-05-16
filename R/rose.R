47b79d5fb7be55e66640a4d575e19bc3a7d1c0a1
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2020-05-15 17:42:54 -0700

    switch stop to rlang::abort

--- R/rose.R
+++ R/rose.R
@@ -147,9 +147,9 @@ prep.step_rose <- function(x, training, info = NULL, ...) {
 
   col_name <- terms_select(x$terms, info = info)
   if (length(col_name) != 1)
-    stop("Please select a single factor variable.", call. = FALSE)
+    rlang::abort("Please select a single factor variable.")
   if (!is.factor(training[[col_name]]))
-    stop(col_name, " should be a factor variable.", call. = FALSE)
+    rlang::abort(paste0(col_name, " should be a factor variable."))
 
   check_2_levels_only(training, col_name)
   check_type(select(training, -col_name), TRUE)
