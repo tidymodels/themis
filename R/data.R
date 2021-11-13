8d41d1f3f099c649238a0cc07196137822805ae9
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-11-12 16:19:25 -0800

    add id variable to circle_example data set

--- R/data.R
+++ R/data.R
@@ -3,10 +3,11 @@
 #' A random dataset with two classes one of which is inside a circle. Used for
 #' examples to show how the different methods handles borders.
 #'
-#' @format A data frame with 200 rows and 3 variables:
+#' @format A data frame with 200 rows and 4 variables:
 #' \describe{
 #'   \item{x}{Numeric.}
 #'   \item{y}{Numeric.}
 #'   \item{class}{Factor, values "Circle" and "Rest".}
+#'   \item{id}{character, ID variable.}
 #' }
 "circle_example"
---
+++ data-raw/circle_example.R
@@ -0,0 +1,15 @@
+set.seed(1234)
+library(dplyr)
+
+circle_example <- data.frame(
+  x = runif(400, 1, 15),
+  y = runif(400, 1, 15)
+) %>%
+  mutate(
+    class = factor(x = sqrt((x - 8)^2 + (y - 8)^2) < 3,
+                   levels = c(TRUE, FALSE),
+                   labels = c("Circle", "Rest")),
+    id = as.character(row_number())
+  )
+
+usethis::use_data(circle_example, overwrite = TRUE)
