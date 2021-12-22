d4f9294c74b4183e2247fbf24e6bef4dc0bb3889
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2021-12-21 18:58:57 -0800

    import enqous

--- R/0_imports.R
+++ R/0_imports.R
@@ -3,7 +3,7 @@
 #' @importFrom purrr map_dfr map_lgl
 #' @importFrom recipes add_step bake check_type ellipse_check is_trained prep
 #' @importFrom recipes printer rand_id sel2char step recipes_eval_select
-#' @importFrom rlang :=
+#' @importFrom rlang := enquos
 #' @importFrom ROSE ROSE
 #' @importFrom tibble as_tibble tibble
 #' @importFrom unbalanced ubTomek
