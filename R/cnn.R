d0262733fea7cb4224ba477119fb853d1bbf7b6f
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-07-16 12:16:39 -0700

    Add step_cnn() for Condensed Nearest Neighbors

---
+++ R/cnn.R
@@ -0,0 +1,259 @@
+#' Condensed Nearest Neighbors
+#'
+#' `step_cnn()` creates a *specification* of a recipe step that removes
+#' redundant majority class observations, keeping only a consistent subset that
+#' correctly classifies the data using a 1-nearest-neighbor rule.
+#'
+#' @inheritParams recipes::step_center
+#' @inheritParams step_nearmiss
+#' @param ... One or more selector functions to choose which
+#'  variable is used to sample the data. See [recipes::selections]
+#'  for more details. The selection should result in _single
+#'  factor variable_. For the `tidy` method, these are not
+#'  currently used.
+#' @param role Not used by this step since no new variables are
+#'  created.
+#' @param column A character string of the variable name that will
+#'  be populated (eventually) by the `...` selectors.
+#' @param seed An integer that will be used as the seed when
+#' applied.
+#' @return An updated version of `recipe` with the new step
+#'  added to the sequence of existing steps (if any). For the
+#'  `tidy` method, a tibble with columns `terms` which is
+#'  the variable used to sample.
+#'
+#' @template details-cnn
+#'
+#' @details
+#' All variables selected by `distance_with` must be numeric with no missing
+#' data.
+#'
+#' All columns in the data are sampled and returned by [recipes::juice()]
+#'  and [recipes::bake()].
+#'
+#' When used in modeling, users should strongly consider using the
+#'  option `skip = TRUE` so that the extra sampling is _not_
+#'  conducted outside of the training set.
+#'
+#' # Tidying
+#'
+#' When you [`tidy()`][recipes::tidy.recipe()] this step, a tibble is returned with
+#'  columns `terms` and `id`:
+#'
+#' \describe{
+#'   \item{terms}{character, the selectors or variables selected}
+#'   \item{id}{character, id of this step}
+#' }
+#'
+#' @template case-weights-not-supported
+#'
+#' @references Hart, P. (1968). The condensed nearest neighbor rule. IEEE
+#' Transactions on Information Theory, 14(3), 515-516.
+#'
+#' @seealso [cnn()] for direct implementation
+#' @family Steps for under-sampling
+#'
+#' @export
+#' @examplesIf rlang::is_installed("modeldata")
+#' library(recipes)
+#' library(modeldata)
+#' data(hpc_data)
+#'
+#' hpc_data0 <- hpc_data |>
+#'   select(-protocol, -day)
+#'
+#' orig <- count(hpc_data0, class, name = "orig")
+#' orig
+#'
+#' up_rec <- recipe(class ~ ., data = hpc_data0) |>
+#'   step_cnn(class) |>
+#'   prep()
+#'
+#' training <- up_rec |>
+#'   bake(new_data = NULL) |>
+#'   count(class, name = "training")
+#' training
+#'
+#' # Since `skip` defaults to TRUE, baking the step has no effect
+#' baked <- up_rec |>
+#'   bake(new_data = hpc_data0) |>
+#'   count(class, name = "baked")
+#' baked
+#'
+#' orig |>
+#'   left_join(training, by = "class") |>
+#'   left_join(baked, by = "class")
+#'
+#' library(ggplot2)
+#'
+#' ggplot(circle_example, aes(x, y, color = class)) +
+#'   geom_point() +
+#'   labs(title = "Without CNN") +
+#'   xlim(c(1, 15)) +
+#'   ylim(c(1, 15))
+#'
+#' recipe(class ~ x + y, data = circle_example) |>
+#'   step_cnn(class) |>
+#'   prep() |>
+#'   bake(new_data = NULL) |>
+#'   ggplot(aes(x, y, color = class)) +
+#'   geom_point() +
+#'   labs(title = "With CNN") +
+#'   xlim(c(1, 15)) +
+#'   ylim(c(1, 15))
+step_cnn <-
+  function(
+    recipe,
+    ...,
+    role = NA,
+    trained = FALSE,
+    column = NULL,
+    distance = "euclidean",
+    skip = TRUE,
+    seed = sample.int(10^5, 1),
+    distance_with = recipes::all_predictors(),
+    id = rand_id("cnn")
+  ) {
+    check_number_whole(seed)
+    check_distance_arg(distance)
+
+    add_step(
+      recipe,
+      step_cnn_new(
+        terms = enquos(...),
+        role = role,
+        trained = trained,
+        column = column,
+        distance = distance,
+        predictors = NULL,
+        skip = skip,
+        seed = seed,
+        distance_with = enquos(distance_with),
+        id = id
+      )
+    )
+  }
+
+step_cnn_new <-
+  function(
+    terms,
+    role,
+    trained,
+    column,
+    distance,
+    predictors,
+    skip,
+    seed,
+    distance_with,
+    id
+  ) {
+    step(
+      subclass = "cnn",
+      terms = terms,
+      role = role,
+      trained = trained,
+      column = column,
+      distance = distance,
+      predictors = predictors,
+      skip = skip,
+      seed = seed,
+      distance_with = distance_with,
+      id = id
+    )
+  }
+
+#' @export
+prep.step_cnn <- function(x, training, info = NULL, ...) {
+  col_name <- recipes_eval_select(x$terms, training, info)
+
+  check_1_selected(col_name)
+  check_column_factor(training, col_name)
+
+  distance_cols <- recipes_argument_select(
+    x$distance_with,
+    training,
+    info,
+    single = FALSE,
+    arg_name = "distance_with"
+  )
+  predictors <- setdiff(distance_cols, col_name)
+
+  check_type(training[, predictors], types = c("double", "integer"))
+  check_na(select(training, all_of(c(col_name, predictors))))
+
+  step_cnn_new(
+    terms = x$terms,
+    role = x$role,
+    trained = TRUE,
+    column = col_name,
+    distance = x$distance,
+    predictors = predictors,
+    skip = x$skip,
+    seed = x$seed,
+    distance_with = x$distance_with,
+    id = x$id
+  )
+}
+
+#' @export
+bake.step_cnn <- function(object, new_data, ...) {
+  col_names <- unique(c(object$predictors, object$column))
+  check_new_data(col_names, object, new_data)
+
+  if (length(object$column) == 0L) {
+    # Empty selection
+    return(new_data)
+  }
+
+  if (nrow(new_data) <= 1) {
+    return(new_data)
+  }
+
+  predictor_data <- new_data[, col_names]
+
+  # cnn with seed for reproducibility
+  with_seed(
+    seed = object$seed,
+    code = {
+      cnn_data <- cnn_impl(
+        df = predictor_data,
+        var = object$column,
+        distance = object$distance
+      )
+    }
+  )
+
+  if (length(cnn_data) > 0) {
+    new_data <- new_data[-cnn_data, ]
+  }
+
+  new_data
+}
+
+#' @export
+print.step_cnn <-
+  function(x, width = max(20, options()$width - 26), ...) {
+    title <- "CNN based on "
+    print_step(x$column, x$terms, x$trained, title, width)
+    invisible(x)
+  }
+
+#' @rdname step_cnn
+#' @usage NULL
+#' @export
+tidy.step_cnn <- function(x, ...) {
+  if (is_trained(x)) {
+    res <- tibble(terms = unname(x$column))
+  } else {
+    term_names <- sel2char(x$terms)
+    res <- tibble(terms = unname(term_names))
+  }
+  res$id <- x$id
+  res
+}
+
+#' @rdname required_pkgs.step
+#' @export
+required_pkgs.step_cnn <- function(x, ...) {
+  c("themis")
+}
