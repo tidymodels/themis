41c900097128115983aa42568449f966a88b8937
Giorgio Maria Federico Birnthaler <129273127+Dodothereal@users.noreply.github.com>
2026-07-20 21:39:40 +0300

    doc: update smotenc and step_smotenc references to include Gower (1971) for distance metric (#278)
    Co-authored-by: Emil Hvitfeldt <emilhhvitfeldt@gmail.com>

--- R/smotenc_impl.R
+++ R/smotenc_impl.R
@@ -22,6 +22,9 @@
 #'  W. P. (2002). Smote: Synthetic minority over-sampling technique.
 #'  Journal of Artificial Intelligence Research, 16:321-357.
 #'
+#'  Gower, J. C. (1971). A general coefficient of similarity and some of
+#'  its properties. Biometrics 27(4):857-871. (For the distance metric used)
+#'
 #' @seealso [step_smotenc()] for step function of this method
 #' @family Direct Implementations
 #'
