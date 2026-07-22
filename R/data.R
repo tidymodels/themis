859cc9fc16f2cab1280216edd4df40419213c21a
Sai Asish Y <say.apm35@gmail.com>
2026-07-22 13:37:39 -0700

    Add @source for circle_example and fix documented row count (#288)
    Signed-off-by: Sai Asish Y <say.apm35@gmail.com>

--- R/data.R
+++ R/data.R
@@ -3,11 +3,12 @@
 #' A random dataset with two classes one of which is inside a circle. Used for
 #' examples to show how the different methods handles borders.
 #'
-#' @format A data frame with 200 rows and 4 variables:
+#' @format A data frame with 400 rows and 4 variables:
 #' \describe{
 #'   \item{x}{Numeric.}
 #'   \item{y}{Numeric.}
 #'   \item{class}{Factor, values "Circle" and "Rest".}
 #'   \item{id}{character, ID variable.}
 #' }
+#' @source Synthetic data, simulated in \code{data-raw/circle_example.R}.
 "circle_example"
