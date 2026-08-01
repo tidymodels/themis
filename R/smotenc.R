5a685d5b8fcdb173a92af060fdbc7ee3ca7ea067
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-08-01 16:38:30 -0700

    don't use step_impute_knn() in examples

--- R/smotenc.R
+++ R/smotenc.R
@@ -77,7 +77,6 @@
 #' orig
 #'
 #' up_rec <- recipe(class ~ ., data = hpc_data) |>
-#'   step_impute_knn(all_predictors()) |>
 #'   # Bring the minority levels up to about 1000 each
 #'   # 1000/2211 is approx 0.4523
 #'   step_smotenc(class, over_ratio = 0.4523) |>
